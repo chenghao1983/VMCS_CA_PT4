0f70b4c33604e95da3fe1138c4482565435107ae
chenghao1983 <joechenghao@gmail.com>
2016-09-30 13:04:14 +0800

    - added CLASSPATH and setenv.bat back
    guys please dont commit these two after you change the config

---
+++ Solution/VMCS_PT4/setenv.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_77
+set ANT_HOME=D:\Download\apache-ant-1.9.7
+set JUNIT_HOME=.\jars
+
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set CLASSPATH=%JUNIT_HOME%\junit.jar
